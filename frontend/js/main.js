9f4071927fe85544850820d8df80008266f71fce
Evan <evanearl@gmail.com>
2016-01-23 18:30:54 -0700

    Populate grid with randomized data

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,9 +4,10 @@ var size = 19;
 
 function test() {
     for (var i = 0; i < size; i++){
+
         data[i] = [];
         for (var j = 0; j < size; j++){
-            data[i][j] = "O";
+            data[i][j] = Math.floor((Math.random() * 3.0) + 1) - 1;
         }
     }
     drawTable(data);
@@ -39,16 +40,16 @@ function drawRow(rowData, currentRow) {
 
     for (var j = 0; j < rowData.length; j++) {
 
-        if ( j == 0) {
-            color = ""
+        if ( rowData[j] == 0) {
+            color = "<img src=img/null.png>"
         }       
-        else if ( j == 1 ) {
+        else if ( rowData[j] == 1 ) {
             color = "<img src=img/black.png>"
         }  
-        else if ( j == 2 ) {
+        else if ( rowData[j] == 2 ) {
             color = "<img src=img/white.png>"
         }
 
-        row.append($("<td>" + "<img src=img/black.png>" + "</td>"));
+        row.append($("<td>" + color + "</td>"));
     }
 }
