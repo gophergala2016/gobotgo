9d8d910d5f5568ec4cd4c076270e9b094976af0c
Evan <evanearl@gmail.com>
2016-01-23 22:51:39 -0700

    Refactor main.js, clean up stylesheet, add board listeners

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,8 @@ var data = [];
 var size = 19;
 var toastValue = "The toast is shown.";
 
-function test() {
+// initialize some sample data and  draw the table containing it
+function init() {
     for (var i = 0; i < size; i++){
 
         data[i] = [];
@@ -14,11 +15,41 @@ function test() {
     drawTable(data);
 }
 
-$('.showToast').click(function () {
-    $('.error').text(toastValue);
-    $('.error').stop().fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
+// temporary listeners - most events will be based on returns from POST requests
+$('#GameBoard').on('click', 'td', function(_evt) {
+    console.log("Clicked", this, _evt);
 });
 
+$('.showToast').click(function () {
+    showToast(toastValue, 3000);
+});
+
+// Activate the temporary notification 'toast' for _time ms with _message
+function showToast(_message, _time) {
+    $('.error').text(_message);
+    $('.error').stop().fadeIn(400).delay(_time).fadeOut(400); //fade out after 3 seconds
+}
+
+function mouseEnter(_fn) {
+    return function(_evt) {
+        var relTarget = _evt.relatedTarget;
+        if (this === relTarget || isAChildOf(this, relTarget)) {
+            return;
+        }
+        _fn.call(this, _evt);
+    }
+}
+
+function isAChildOf(_parent, _child) {
+    if (_parent === _child) { return false; }
+
+    while (_child && _child !== _parent) { 
+        _child = _child.parentNode;
+    }
+    return _child === _parent;
+}
+
+// Render the board
 function drawTable(data) {
     var header = $("<tr>")
     $("#GameBoard").append(header);
@@ -36,13 +67,14 @@ function drawTable(data) {
     }
 }
 
+// Generate one full row given the data and the row to be generated
 function drawRow(rowData, currentRow) {
 
     var color = ""
     var row = $("<tr />")
+
     $("#GameBoard").append(row);
     row.append($("<td>" + String.fromCharCode(65+currentRow) + "</td>"));
-
 
     for (var j = 0; j < rowData.length; j++) {
 
