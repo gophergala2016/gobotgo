f7fdedae55197b83de9bea51d8a1824962ff1bd8
Evan <evanearl@gmail.com>
2016-01-23 18:05:33 -0700

    Create javascript to populate table

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,14 @@
 
-var data = [
-    ["e", "e", "e"],
-    ["e", "e", "e"],
-    ["e", "e", "e"]
-];
+var data = [];
+var size = 19;
 
 function test() {
+    for (var i = 0; i < size; i++){
+        data[i] = [];
+        for (var j = 0; j < size; j++){
+            data[i][j] = "O";
+        }
+    }
     drawTable(data);
 }
 
@@ -13,7 +16,7 @@ function drawTable(data) {
     var header = $("<tr>")
     $("#GameBoard").append(header);
 
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length+1; i++) {
         header.append("<th>" + i + "</th>");
     }
 
@@ -22,15 +25,30 @@ function drawTable(data) {
     $("GameBoard").append("<tr>");
 
     for (var i = 0; i < data.length; i++) {
-        drawRow(data[i]);
+        drawRow(data[i], i);
     }
 }
 
-function drawRow(rowData) {
+function drawRow(rowData, currentRow) {
+
+    var color = ""
     var row = $("<tr />")
-    
+    $("#GameBoard").append(row);
+    row.append($("<td>" + String.fromCharCode(65+currentRow) + "</td>"));
+
+
     for (var j = 0; j < rowData.length; j++) {
-        $("#GameBoard").append(row);
-        row.append($("<td>" + rowData[j] + "</td>"));
+
+        if ( j == 0) {
+            color = ""
+        }       
+        else if ( j == 1 ) {
+            color = "<img src=img/black.png>"
+        }  
+        else if ( j == 2 ) {
+            color = "<img src=img/white.png>"
+        }
+
+        row.append($("<td>" + "<img src=img/black.png>" + "</td>"));
     }
 }
